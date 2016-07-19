73c8b6b36b544588040d68b2d4f511546f851181
cato-hga <jchristopher.cato@gmail.com>
2016-07-18 22:58:52 -0400

    Create migration for Recipe model

---
+++ app/models/recipe.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Recipe < ActiveRecord::Base
+end
